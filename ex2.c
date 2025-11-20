35c49aa0db5037ef6d29bf20693dc4d4d9770b95
halel <halel921@gmail.com>
2025-11-20 21:58:49 +0200

    create menu

--- ex2.c
+++ ex2.c
@@ -9,6 +9,25 @@ Assignment:
 int main()
 {
     // MAIN MENU LOOP
+    int option=2;
+
+   do {
+       if (1> option || option >7) {
+           printf("Invalid option, please try again\n");
+       }
+
+       printf("Welcome to our games, please choose an option:\n");
+       printf("1. Ducky's Unity Game\n");
+       printf("2. The Memory Game\n");
+       printf("3. Professor Pat's Power Calculation\n");
+       printf("4. The Duck Parade\n");
+       printf("5. The Mystery of the Repeated Digits\n");
+       printf("6. Good Night Ducks\n");
+
+    scanf("%d",&option);
+   }while (1> option || option >7);
+
+
 
     // TASK 1: Ducky's Unity Game
 
