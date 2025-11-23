f52a475d32a12433d33412b6247073691aed3091
halel <halel921@gmail.com>
2025-11-23 19:57:32 +0200

    cases 1-6

--- ex2.c
+++ ex2.c
@@ -4,6 +4,8 @@ ID: 211560065
 Assignment: ex2
 *******************/
 
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 
 int main() {
@@ -30,7 +32,6 @@ int main() {
                 scanf("%d",&PositiveNumber);
                 while (PositiveNumber < 1) {
                     printf("Invalid number, please try again\n");
-                    printf("please enter a positive number:\n");
                     scanf("%d",&PositiveNumber);
                 }
                 while (PositiveNumber > 0) {
@@ -40,22 +41,105 @@ int main() {
                 printf("Ducky earns %d corns", count);
             }
                 break;
-            // TASK 2: The Memory Game
+                // TASK 2: The Memory Game
             case 2: {
                 int DucksNumber;
+                long quak, quaks=0;
                 printf("please enter the number of ducks:\n");
                 scanf("%d",&DucksNumber);
-                printf("you entered %d ducks", DucksNumber);
+                while (DucksNumber < 1) {
+                    printf("Invalid number, please try again\n");
+                    scanf("%d",&DucksNumber);
+                }
+                printf("you entered %d ducks\n", DucksNumber);
+                for (int i = 0; i<DucksNumber; i++) {
+                    printf("duck %d do QUAK? 1 for yes, 0 for no\n", i+1);
+                    scanf("%ld",&quak);
+                    while (quak !=0 && quak !=1) {
+                        printf("Invalid number, please try again\n");
+                        scanf("%ld",&quak);
+                    }
+                    if (quak == 1) {
+                        quaks |= (1ull << i);
+                    }
+                }
+                for (int i = 0; i<DucksNumber; i++) {
+                    if (quaks & (1ull << i)) {
+                        printf("duck number %d do Quak\n", i+1);
+                    } else {
+                        printf("duck number %d do Sh...\n", i+1);
+                    }
+
+                }
+
+            }
+
+                break;
+                // TASK 3: Professor Pat's Power Calculation
+            case 3: {
+                int number, exponent, power=1;
+                printf("please enter the number\n");
+                scanf("%d",&number);
+                while (number < 0) {
+                    printf("Invalid number, please try again\n");
+                    scanf("%d",&number);
+                }
+                printf("please enter the exponent\n");
+                scanf("%d",&exponent);
+                while (exponent < 0) {
+                    printf("Invalid number, please try again\n");
+                    scanf("%d",&exponent);
+                }
+                if (exponent == 0 && number == 0) {
+                    power = 1;
+                } else {
+                    for (int i = 0; i<exponent; i++) {
+                        power = power * number;
+                    }
+                }
+                printf("your power is:%d", power);
+            }
+                    break;
+            // TASK 4: The Duck Parade
+            case 4: {
+                int NumberOfDucks;
+                printf("please enter number of ducks:\n");
+                scanf("%d",&NumberOfDucks);
+                while (NumberOfDucks < 0) {
+                    printf("Invalid number, please try again\n");
+                    scanf("%d",&NumberOfDucks);
+                }
+                if (NumberOfDucks == 0) {
+                    break; //משהו לא עובד
+                } else {
+                    for (int i = 0; i<NumberOfDucks; i++) {
+                        printf("   _\t\t");
+                    }
+                    for (int i = 0; i<NumberOfDucks; i++) {
+                        printf("__(o)>\t\t");
+                    }
+                    for (int i = 0; i<NumberOfDucks; i++) {
+                        printf("\\___)\t\t");
+                    }
+                    if ((NumberOfDucks+1)%10 == 0) {
+                        printf("\n");
+                    }
+                }
             }
                 break;
-            // TASK 3: Professor Pat's Power Calculation
-            case 3:
-                break;
-            // TASK 4: The Duck Parade
-            case 4:
-                break;
             // TASK 5: The Mystery of the Repeated Digits
-            case 5:
+            case 5: {
+                int Digit;
+                printf("please enter number\n");
+                scanf("%d",&Digit);
+                while (Digit < 1) {
+                    printf("Invalid number, please try again\n");
+                    scanf("%d",&Digit);
+                }
+                while (Digit > 1) {
+                    printf("%d appears more than once!", Digit);
+                }
+            }
                 break;
             // TASK 6: EXIT
             case 6:
@@ -66,20 +150,6 @@ int main() {
                 break;
         }
     }while (1> option || option >6);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // dont forget through all the necessary parts to validate invalid input.
 
